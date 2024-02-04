945fd13cb20632a22a883603ae669eca29924836
alex <al3xback@gmail.com>
2024-02-04 14:37:13 +0700

    added alt image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,7 @@ const Card = () => {
 	return (
 		<article className={styles.card}>
 			<div className={styles['card__image']}>
-				<img width="302" height="302" src={equilibriumImage} alt="" />
+				<img width="302" height="302" src={equilibriumImage} alt="Equilibrium" />
 			</div>
 			<div className={styles['card__content']}>
 				<h2 className={styles['card__title']}>
